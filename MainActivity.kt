594a779b1d4ea505cab50aa553fd007f06c3618f
Uygar Muntaş <muntasuygar@gmail.com>
2023-07-23 20:44:54 +0300

    first commit

---
+++ MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myfirstapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnClickMe = findViewById<Button>(R.id.button1)
+        val helloDudeButton = findViewById<TextView>(R.id.textView1)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "you clicked me !"
+            helloDudeButton.text = "you clicked button and button switched me "
+
+        }
+
+
+
+
+
+    }
+
+
+}
